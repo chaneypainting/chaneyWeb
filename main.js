f46bf8d62f1e50e8e64927a906ba408e545bf39e
raykrow <ray.krow@yahoo.com>
2016-06-11 20:43:38 -0700

    redo of landing

--- main.js
+++ main.js
@@ -16,19 +16,120 @@ var Chaney = {
     formValInt: null,
     $valRiddle: $('#validationRiddle'),
     $valAnswer: $('#validationAnswer'),
-    $valResponse: $('#validationResponse')
+    $valResponse: $('#validationResponse'),
+    $landingContent: $('.cp-landing-content'),
+    landingHeight: null,
+    windowHeight: null,
+    $window: $(window),
+    $landingRoller: $('#roller'),
+    $rollerElem: null,
+    rollerHeight: null,
+    testMessages: [],
+    testMessageIndex: 1,
+    testMessageLength: null
 }
 
 $(function() {
 
+    // Cache DOM & Values
     Chaney.galleryClosedHeight = Chaney.$gallery.css('maxHeight');
     Chaney.galleryOpenHeight = Chaney.$gallery.children().first().innerHeight();
+    Chaney.windowHeight = window.innerHeight;
+    Chaney.landingHeight = $('.cp-landing-section').innerHeight() * .50;
+    Chaney.rollerHeight = Chaney.$landingRoller.innerHeight();
+    createTestimonialArray();
+    Chaney.testMessageLength = Chaney.testMessages.length;
+    Chaney.$rollerElem = $('#roller > blockquote');
 
+    // Init Listeners
     $('[data-goto]').click(gotoElem);
+    // $(window).scroll(throttle(onWindowScroll, 100, this));
+    // $(window).scroll(onWindowScroll);
 
+    // Kick Off Functions
     generateFormValidationRiddle();
+    setInterval(rollLanding, 7000);
 
 });
+
+function createTestimonialArray() {
+
+    Chaney.$landingRoller.children('blockquote').each(function(i, elem) {
+        var $elem = $(elem);
+        Chaney.testMessages.push($elem.html());
+        if (i != 0) { $elem.remove(); }
+    });
+
+}
+
+function rollLanding() {
+
+    var animateTime = 750;
+
+    Chaney.$rollerElem.animate({
+        top: -Chaney.rollerHeight
+    }, animateTime, function() {
+
+        // Animation complete.
+        Chaney.$rollerElem
+        .hide()
+        .html(getNextRollerMessage())
+        .css('top', Chaney.rollerHeight)
+        .show()
+        .animate({
+            top: 0
+        }, animateTime);
+
+    });
+
+};
+
+function getNextRollerMessage() {
+
+    var i = Chaney.testMessageIndex,
+        test = i + 1;
+
+    if (test >= Chaney.testMessageLength) {
+        Chaney.testMessageIndex = 0;
+    } else {
+        Chaney.testMessageIndex++;
+    }
+
+    console.log("Array Indexed at: ", i);
+    return Chaney.testMessages[i];
+};
+
+function onWindowScroll() {
+
+    var distFromTop = Chaney.$window.scrollTop();
+    if (distFromTop > Chaney.landingHeight) { return; }
+
+    Chaney.$landingContent.css('opacity', (1 - (distFromTop / Chaney.landingHeight)));
+
+};
+
+function throttle(fn, threshhold, scope) {
+  threshhold || (threshhold = 250);
+  var last,
+      deferTimer;
+  return function () {
+    var context = scope || this;
+
+    var now = +new Date,
+        args = arguments;
+    if (last && now < last + threshhold) {
+      // hold on to it
+      clearTimeout(deferTimer);
+      deferTimer = setTimeout(function () {
+        last = now;
+        fn.apply(context, args);
+      }, threshhold);
+    } else {
+      last = now;
+      fn.apply(context, args);
+    }
+  };
+}
 
 function generateFormValidationRiddle() {
 
